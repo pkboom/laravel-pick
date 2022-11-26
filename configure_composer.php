656e508596c682bac0a2c2000714a849438798d2
Keunbae Park <pkboom@users.noreply.github.com>
2022-11-26 13:52:31 -0500

    wip

--- configure_composer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$path = getcwd().'/composer.json';
-
-$composer = json_decode(file_get_contents($path), true);
-
-$composer['autoload']['psr-4']["Pkboom\\Pick\\"] = "src/";
-
-file_put_contents($path, json_encode($composer, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
