e810b4977e2872b511a9e9dbe7419b23f2426d2e
Keunbae Park <pkboom@users.noreply.github.com>
2024-04-11 10:12:30 -0400

    wip

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,35 +1,18 @@
 <?php
 
-return (new PhpCsFixer\Config())
+use PhpCsFixer\Config;
+
+return (new Config())
     ->setRules([
-        '@PSR12' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        'ordered_imports' => ['sort_algorithm' => 'alpha'],
-        'no_unused_imports' => true,
+        '@PhpCsFixer' => true,
         'not_operator_with_successor_space' => true,
-        'trailing_comma_in_multiline' => true,
-        'phpdoc_scalar' => true,
-        'unary_operator_spaces' => true,
-        'binary_operator_spaces' => true,
         'blank_line_before_statement' => [
             'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
         ],
-        'phpdoc_single_line_var_spacing' => true,
-        'phpdoc_var_without_name' => true,
-        'class_attributes_separation' => [
-            'elements' => [
-                'method' => 'one',
-            ],
-        ],
-        'method_argument_space' => [
-            'on_multiline' => 'ensure_fully_multiline',
-            'keep_multiple_spaces_after_comma' => true,
-        ],
-        'single_trait_insert_per_statement' => true,
-        'no_extra_blank_lines' => [
-            'tokens' => [
-                'extra',
-            ],
-        ],
-        'no_singleline_whitespace_before_semicolons' => true,
+        'multiline_whitespace_before_semicolons' => true,
+        'php_unit_method_casing' => ['case' => 'snake_case'],
+        'php_unit_test_class_requires_covers' => false,
+        'php_unit_internal_class' => false,
+        'global_namespace_import' => true,
+        'phpdoc_add_missing_param_annotation' => false,
     ]);
